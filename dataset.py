455df555556b651ed87c2374fd151754b205b141
Mostro <bright196@163.com>
2020-01-24 01:00:44 +0800

    fusion is successful

--- dataset.py
+++ dataset.py
@@ -1,6 +1,5 @@
 import json
 import os
-from abc import ABCMeta, abstractmethod
 
 import cv2
 import h5py
@@ -10,17 +9,7 @@ import torchvision
 from torchvision.transforms import Compose, Resize
 
 
-class PreprocessPipeline(metaclass=ABCMeta):
-    @abstractmethod
-    def skeleton2img(self, mat):
-        pass
-
-    @abstractmethod
-    def standardize_pose(self, mat, resize_isize):
-        pass
-
-
-class MSRAction3D(torch.utils.data.Dataset, PreprocessPipeline):
+class MSRAction3D(torch.utils.data.Dataset):
     def __init__(self, root, method, split_id=0, resize_isize=(60, 60, 3)):
         super(MSRAction3D, self).__init__()
         self.method = method
@@ -84,9 +73,7 @@ class MSRAction3D(torch.utils.data.Dataset, PreprocessPipeline):
         return features, labels
 
     def __getitem__(self, index):
-        feature = self.skeleton2img(self.features[index])
-        feature = self.standardize_pose(feature, self.resize_isize)
-        feature = feature.permute(2, 0, 1)
+        feature = self.features[index]
         label = self.labels[index]
 
         return feature, label
@@ -94,31 +81,3 @@ class MSRAction3D(torch.utils.data.Dataset, PreprocessPipeline):
     def __len__(self):
         return self.n_features
 
-    def skeleton2img(self, mat):
-        '''map skeletal action to rgb image
-        :param mat: (n_frames, feat_dim)
-        :return: image with resize_isize
-        '''
-        # mat = np.reshape(mat, newshape=(-1, 20, 3))
-        mat = mat.transpose(0, 1)  # mat: (n_feat, n_frames, n_dim)
-
-        n_frames = mat.shape[1]
-        # part_config = [25, 12, 24, 11, 10, 9, 21, 21, 5, 6, 7, 8, 22, 23,
-        #                21, 3, 4, 21, 2, 1, 17, 18, 19, 20, 21, 2, 1, 13, 14, 15, 16]
-        part_config = torch.tensor([12, 10, 8, 1, 1, 3, 2, 2, 9, 11, 13, 20, 3, 4, 7, 7,
-                                    5, 6, 5, 14, 16, 18, 6, 15, 17, 19])
-        mat = mat[part_config - 1]
-
-        return mat
-
-    def standardize_pose(self, mat, resize_isize):
-        '''standardize each pixel of this image
-        '''
-        local_max = torch.max(mat)
-        local_min = torch.min(mat)
-        mat = (mat-local_min)/(local_max-local_min)
-
-        rgb_image = cv2.resize(mat.numpy(), resize_isize[:2])
-        # cv2.imshow('123',rgb_image)
-        # cv2.waitKey()
-        return torch.tensor(rgb_image)
--- model.py
+++ model.py
@@ -2,12 +2,15 @@ import torch
 from dataset import MSRAction3D
 from torch.utils.data.dataloader import DataLoader
 from torch.nn import Conv2d, MaxPool2d, Linear, BatchNorm2d, Dropout2d
-from torch.nn import CrossEntropyLoss
+from torch.nn import CrossEntropyLoss, BatchNorm1d
 from torch.nn.functional import dropout2d, pad
 from torch.nn.functional import relu, softmax
 from torch.optim import SGD
+from torch.nn.parameter import Parameter
+from torch.nn.init import xavier_normal
 import numpy as np
 from abc import ABCMeta, abstractmethod
+from tqdm import tqdm
 
 
 def same_padding(input, kernel_size, stride=None, dilation=1):
@@ -38,17 +41,14 @@ def same_padding(input, kernel_size, stride=None, dilation=1):
 
 
 class TrainableModule(metaclass=ABCMeta):
-    def __init__(self, path=None, batch_size=32):
-        self.path = path
+    def __init__(self, predict_mode=False, batch_size=32):
+        self.predict_mode = predict_mode
         self.batch_size = batch_size
         self.train_loader = None
         self.test_loader = None
         self.n_steps = 0
 
-        if self.path is not None:
-            self = torch.load(path)
-
-    def set_dataloder(self, method, data_loader):
+    def set_dataloader(self, method, data_loader):
         if method == 'train':
             self.train_loader = data_loader
             self.n_steps = 1+len(data_loader)//self.batch_size
@@ -66,7 +66,7 @@ class TrainableModule(metaclass=ABCMeta):
         pass
 
     @abstractmethod
-    def train_module(self, epochs):
+    def train_module(self, epochs, l2_regular_lambda=1e-2):
         pass
 
     @abstractmethod
@@ -79,11 +79,13 @@ class TrainableModule(metaclass=ABCMeta):
 
 
 class ActionConvNet(torch.nn.Module, TrainableModule):
-    def __init__(self, path=None, regular_lambda=1e-2):
+    def __init__(self, input_isize=(60, 60, 3), predict_mode=False):
         torch.nn.Module.__init__(self)
-        TrainableModule.__init__(self, path=path)
-        self.regular_lambda = regular_lambda
-        self.conv1 = Conv2d(in_channels=3,
+        TrainableModule.__init__(self, predict_mode=predict_mode)
+
+        self.input_isize = input_isize
+        self.n_hidden = 256
+        self.conv1 = Conv2d(in_channels=input_isize[2],
                             out_channels=32,
                             kernel_size=3,
                             stride=1)
@@ -114,8 +116,8 @@ class ActionConvNet(torch.nn.Module, TrainableModule):
         self.bn2 = BatchNorm2d(64, eps=1e-3, momentum=0.99,
                                track_running_stats=False)
         self.dropout2 = Dropout2d(0.5)
-        self.fc1 = Linear(in_features=256, out_features=256)
-        self.fc2 = Linear(in_features=256, out_features=20)
+        self.fc1 = Linear(in_features=256, out_features=self.n_hidden)
+        self.fc2 = Linear(in_features=self.n_hidden, out_features=20)
 
     def forward(self, x):
         # input, kernal_size, padding
@@ -138,24 +140,27 @@ class ActionConvNet(torch.nn.Module, TrainableModule):
 
         x = self.bn2(x)
         x = self.dropout2(x)
-
-        x = x.view(-1, self.get_n_features(x))
+        x = x.view(-1, self.get_hidden_size(x))
         x = relu(self.fc1(x))
-        x = self.fc2(x)  # TODO:正则化？
+        if self.predict_mode:
+            x = self.fc2(x)  # TODO:正则化？
         return x
 
-    def get_n_features(self, x):
+    def get_hidden_size(self, x):
         size = np.array(x.size())
         n_features = np.prod(size[1:])
 
         return n_features
 
-    def train_module(self, epochs):
+    def train_module(self, epochs, l2_regular_lambda=1e-2):
+        assert self.predict_mode, "You shouldn't use this method in predict mode"
+
         for epoch in range(epochs):
             self.train(True)
             train_loss = 0.0
             n_correct_preds, n_total_preds = 0, 0
-            for i, data in enumerate(self.train_loader, 0):
+            train_loader = tqdm(self.train_loader, ncols=90, unit='batchs')
+            for i, data in enumerate(train_loader, 0):
                 inputs, labels = data
                 # zero the parameter gradients
                 self.optimizer.zero_grad()
@@ -165,8 +170,8 @@ class ActionConvNet(torch.nn.Module, TrainableModule):
                 outputs = torch.argmax(categorical_outputs, 1)
 
                 loss = self.criterion(categorical_outputs, labels)
-                loss += self.regular_lambda*(torch.norm(self.fc1.weight, p=2) +
-                                             torch.norm(self.fc2.weight, p=2))
+                loss += l2_regular_lambda*(torch.norm(self.fc1.weight, p=2) +
+                                           torch.norm(self.fc2.weight, p=2))
                 loss.backward()
                 self.optimizer.step()
 
@@ -178,26 +183,26 @@ class ActionConvNet(torch.nn.Module, TrainableModule):
 
                 if i % self.n_steps == self.n_steps-1:    # print every 2000 mini-batches
                     self.show_statistics(
-                        'train', epoch, train_loss, total_accuracy)
+                        train_loader, 'train', epoch, train_loss, total_accuracy)
                     train_loss = 0.0
 
-            self.show_statistics('both', epoch, train_loss, total_accuracy)
+            self.show_statistics(train_loader, 'both',
+                                 epoch, train_loss, total_accuracy)
 
         print('Finished Training')
 
-    def show_statistics(self, method, epoch, loss, accuracy):
+    def show_statistics(self, train_loader, method, epoch, loss, accuracy):
         tr_info = 'epoch: %d loss: %.3f acc: %.3f' % (
             epoch + 1,
             loss / self.n_steps,
             accuracy)
-        if method == 'train':
-            print(tr_info)
-        elif method == 'both':
+        train_loader.set_description(tr_info)
+        if method == 'both':
             val_loss, val_acc = self.evaluate(self.test_loader)
             val_info = 'val_loss: %.3f val_acc: %.3f' % (
                 val_loss, val_acc
             )
-            print(' '.join([tr_info, val_info]))
+            print(val_info)
 
     def evaluate(self, data_loader):
         self.eval()
@@ -224,26 +229,170 @@ class ActionConvNet(torch.nn.Module, TrainableModule):
     def save_module(self, path):
         torch.save(self, path)
 
+    def load_module(self, path):
+        checkpoint = torch.load(path)
+        self.load_state_dict(checkpoint)
+
+
+class MultiModalFusion(torch.nn.Module, TrainableModule):
+    def __init__(self, path=None):
+        torch.nn.Module.__init__(self)
+        TrainableModule.__init__(self)
+        if path is not None:
+            checkpoint = torch.load(path)
+            self.load_state_dict(checkpoint)
+        self.n_rank = 1
+        self.n_outputs = 20
+        self.convnet1 = ActionConvNet(predict_mode=True)
+        self.convnet2 = ActionConvNet(predict_mode=True)
+        self.convnet3 = ActionConvNet(input_isize=(60, 60, 1),
+                                      predict_mode=True)
+
+        self.convnet1.set_criterion(CrossEntropyLoss())
+        self.convnet1.set_optimizer(SGD(self.convnet1.parameters(),
+                                        lr=0.01, momentum=0.9, weight_decay=1e-4, nesterov=True))
+        self.convnet2.set_criterion(CrossEntropyLoss())
+        self.convnet2.set_optimizer(SGD(self.convnet2.parameters(),
+                                        lr=0.01, momentum=0.9, weight_decay=1e-4, nesterov=True))
+        self.convnet3.set_criterion(CrossEntropyLoss())
+        self.convnet3.set_optimizer(SGD(self.convnet3.parameters(),
+                                        lr=0.01, momentum=0.9, weight_decay=1e-4, nesterov=True))
+
+        n_hidden_1 = self.convnet1.n_hidden
+        n_hidden_2 = self.convnet2.n_hidden
+        n_hidden_3 = self.convnet3.n_hidden
+
+        self.bn1 = BatchNorm1d(n_hidden_1, eps=1e-3, momentum=0.99,
+                               track_running_stats=False)
+        self.bn2 = BatchNorm1d(n_hidden_2, eps=1e-3, momentum=0.99,
+                               track_running_stats=False)
+        self.bn3 = BatchNorm1d(n_hidden_3, eps=1e-3, momentum=0.99,
+                               track_running_stats=False)
+
+        # abs_pos_factor = Parameter(
+        #     torch.FloatTensor(self.n_rank, n_hidden_1, self.n_outputs))
+        # lp_ang_factor = Parameter(
+        #     torch.FloatTensor(self.n_rank, n_hidden_2, self.n_outputs))
+        # ll_ang_factor = Parameter(
+        #     torch.FloatTensor(self.n_rank, n_hidden_3, self.n_outputs))
+        self.fc1 = Linear(256, 256, bias=False)
+        self.fc2 = Linear(256, 20)
+
+        # fusion_weights = Parameter(torch.FloatTensor(1, self.n_rank))
+        # fusion_bias = Parameter(torch.FloatTensor(1, self.n_outputs))
+
+        # self.abs_pos_factor = xavier_normal(abs_pos_factor)
+        # self.lp_ang_factor = xavier_normal(lp_ang_factor)
+        # self.ll_ang_factor = xavier_normal(ll_ang_factor)
+        # self.fusion_weights = xavier_normal(fusion_weights)
+        # self.fusion_bias = xavier_normal(fusion_bias)
+
+    def forward(self, abs_pos, lp_ang, ll_ang):
+        self.convnet1.predict_mode = False
+        abs_pos = self.convnet1(abs_pos)
+        self.convnet2.predict_mode = False
+        lp_ang = self.convnet2(lp_ang)
+        self.convnet3.predict_mode = False
+        ll_ang = self.convnet3(ll_ang)
+
+        # abs_pos = torch.matmul(abs_pos, self.abs_pos_factor)
+        # lp_ang = torch.matmul(lp_ang, self.lp_ang_factor)
+        # ll_ang = torch.matmul(ll_ang, self.ll_ang_factor)
+
+        fusion_output = abs_pos*lp_ang*ll_ang
+        # fusion_output = fusion_output.permute(1, 0, 2)
+        # fusion_output = torch.matmul(self.fusion_weights, fusion_output)
+        # fusion_output = fusion_output.squeeze()+self.fusion_bias
+        # fusion_output = fusion_output.view(-1, self.n_outputs)
+        fusion_output = self.fc2(fusion_output)
+        return fusion_output
+
+    def train_module(self, epochs, l2_regular_lambda=1e-2):
+        self.convnet1.train_module(epochs=100)
+        self.convnet2.train_module(epochs=100)
+        self.convnet3.train_module(epochs=100)
+        self.convnet1.predict_mode = False
+        self.convnet2.predict_mode = False
+        self.convnet3.predict_mode = False
+        print("Start training fusion model.")
+        for epoch in range(epochs):
+            self.train(True)
+            train_loss = 0.0
+            n_correct_preds, n_total_preds = 0, 0
+            train_loader = tqdm(self.train_loader, ncols=90, unit='batchs')
+            for i, data in enumerate(train_loader, 0):
+                abs_pos, lp_ang, ll_ang, labels = data
+                # zero the parameter gradients
+                self.optimizer.zero_grad()
+
+                # forward + backward + optimize
+                categorical_outputs = self(abs_pos, lp_ang, ll_ang)
+                outputs = torch.argmax(categorical_outputs, 1)
+
+                loss = self.criterion(categorical_outputs, labels)
+                loss += l2_regular_lambda * \
+                    torch.norm(self.fusion_weights, p=2)
+                loss.backward()
+                self.optimizer.step()
+
+                # print statistics
+                train_loss += loss.item()
+                n_correct_preds += torch.sum(labels == outputs).item()
+                n_total_preds += labels.numel()
+                total_accuracy = n_correct_preds / n_total_preds
+
+                if i % self.n_steps == self.n_steps-1:    # print every 2000 mini-batches
+                    self.show_statistics(
+                        train_loader, 'train', epoch, train_loss, total_accuracy)
+                    train_loss = 0.0
+
+            self.show_statistics(train_loader, 'both',
+                                 epoch, train_loss, total_accuracy)
+
+        print('Finished training')
+
+    def show_statistics(self, train_loader, method, epoch, loss, accuracy):
+        tr_info = 'epoch: %d loss: %.3f acc: %.3f' % (
+            epoch + 1,
+            loss / self.n_steps,
+            accuracy)
+        train_loader.set_description(tr_info)
+        if method == 'both':
+            val_loss, val_acc = self.evaluate(self.test_loader)
+            val_info = 'val_loss: %.3f val_acc: %.3f' % (
+                val_loss, val_acc
+            )
+            print(val_info)
+
+    def evaluate(self, data_loader):
+        self.eval()
+        val_loss = 0
+        n_correct_val, n_total_val = 0, 0
+        n_steps = 1+len(data_loader)//self.batch_size
+        with torch.no_grad():
+            for i, data in enumerate(data_loader):
+                abs_pos, lp_ang, ll_ang, labels = data
+                categorical_outputs = self(abs_pos, lp_ang, ll_ang)
+                outputs = torch.argmax(categorical_outputs, 1)
+
+                loss = self.criterion(categorical_outputs, labels)
+                val_loss += loss.item()
+                n_correct_val += torch.sum(labels == outputs).item()
+                n_total_val += labels.numel()
+
+            total_accuracy = n_correct_val/n_total_val
+        return val_loss / len(data_loader), total_accuracy
+
+    def predict(self):
+        pass
+
+    def save_module(self, path):
+        torch.save(self.state_dict(), path)
+
+    def load_module(self, path):
+        checkpoint = torch.load(path)
+        self.load_state_dict(checkpoint)
+
 
 if __name__ == "__main__":
-    batch_size = 32
-
-    train_data = MSRAction3D(root='data/MSRAction3D',
-                             method='train', resize_isize=(52, 52, 3))
-    test_data = MSRAction3D(root='data/MSRAction3D',
-                            method='test', resize_isize=(52, 52, 3))
-
-    train_loader = DataLoader(
-        dataset=train_data, batch_size=batch_size, shuffle=True)
-    test_loader = DataLoader(dataset=test_data, batch_size=batch_size)
-
-    convnet = ActionConvNet()
-    print(convnet)
-
-    convnet.set_dataloder('train', train_loader)
-    convnet.set_dataloder('test', test_loader)
-    convnet.set_criterion(CrossEntropyLoss())
-    convnet.set_optimizer(SGD(convnet.parameters(),
-                              lr=0.01, momentum=0.9, weight_decay=1e-4, nesterov=True))
-    convnet.train_module(epochs=100)
-    convnet.save_module('model/rbg_mapping.pth')
+    pass
